233a8c446183b87255a6ae02df56e628d9d4f8e0
yjn <2297957429@qq.com>
2018-11-07 10:37:08 +0800

    首次提交

---
+++ pages/index/index.js
@@ -0,0 +1,40 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imgslist: [{
+        img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541509077115&di=8c66bd425c3839069768e44d06417a40&imgtype=0&src=http%3A%2F%2Fwww.znsfagri.com%2Fuploadfile%2Feditor%2Fimage%2F20170626%2F20170626151136_11631.jpg',
+        text: '纪梵希高定工坊四色睫毛膏纪版本的放入润肤乳'
+      },
+      {
+        img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541509077115&di=8c66bd425c3839069768e44d06417a40&imgtype=0&src=http%3A%2F%2Fwww.znsfagri.com%2Fuploadfile%2Feditor%2Fimage%2F20170626%2F20170626151136_11631.jpg',
+        text: '纪梵希高定工坊四色睫毛膏'
+      },
+      {
+        img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541509077115&di=8c66bd425c3839069768e44d06417a40&imgtype=0&src=http%3A%2F%2Fwww.znsfagri.com%2Fuploadfile%2Feditor%2Fimage%2F20170626%2F20170626151136_11631.jpg',
+        text: '纪梵希高定工坊'
+      },
+      {
+        img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541509077115&di=8c66bd425c3839069768e44d06417a40&imgtype=0&src=http%3A%2F%2Fwww.znsfagri.com%2Fuploadfile%2Feditor%2Fimage%2F20170626%2F20170626151136_11631.jpg',
+        text: '纪梵希高定工坊四色睫毛膏'
+      },
+      {
+        img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541509077115&di=8c66bd425c3839069768e44d06417a40&imgtype=0&src=http%3A%2F%2Fwww.znsfagri.com%2Fuploadfile%2Feditor%2Fimage%2F20170626%2F20170626151136_11631.jpg',
+        text: '纪梵希高定工坊四色睫毛膏纪'
+      },
+      {
+        img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541509077115&di=8c66bd425c3839069768e44d06417a40&imgtype=0&src=http%3A%2F%2Fwww.znsfagri.com%2Fuploadfile%2Feditor%2Fimage%2F20170626%2F20170626151136_11631.jpg',
+        text: '纪梵希高定工坊四色睫毛膏纪'
+      },
+      {
+        img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541509077115&di=8c66bd425c3839069768e44d06417a40&imgtype=0&src=http%3A%2F%2Fwww.znsfagri.com%2Fuploadfile%2Feditor%2Fimage%2F20170626%2F20170626151136_11631.jpg',
+        text: '纪梵希高定工坊四色睫毛膏纪'
+      }
+    ],
+  },
+  onLoad: function() {
+
+  },
+})
